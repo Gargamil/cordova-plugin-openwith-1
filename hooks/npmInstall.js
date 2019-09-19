f9e7030c8d64f9fc41b710b04e8f14e147b4796a
Tobias Neumann <Gargamil@users.noreply.github.com>
2019-09-19 18:17:32 +0200

    Update npmInstall.js

--- hooks/npmInstall.js
+++ hooks/npmInstall.js
@@ -21,17 +21,17 @@
 const PLUGIN_ID = "cordova-plugin-openwith";
 
 module.exports = function (context) {
-    var child_process = context.requireCordovaModule('child_process');
-    var deferral = context.requireCordovaModule('q').defer();
+  var child_process = require('child_process');
+  var deferral = require('q').defer();
 
-    console.log('Installing "' + PLUGIN_ID + '" dependencies');
-    child_process.exec('npm install --production', {cwd:__dirname}, function (error) {
-        if (error !== null) {
-            console.log('exec error: ' + error);
-            deferral.reject('npm installation failed');
-        }
-        deferral.resolve();
-    });
+  console.log('Installing "' + PLUGIN_ID + '" dependencies');
+  child_process.exec('npm install --production', {cwd: __dirname}, function (error) {
+    if (error !== null) {
+      console.log('exec error: ' + error);
+      deferral.reject('npm installation failed');
+    }
+    deferral.resolve();
+  });
 
-    return deferral.promise;
+  return deferral.promise;
 };
